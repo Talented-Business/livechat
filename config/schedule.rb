015d761d2586d678931f4e1a2d212413c672a47d
sungil <chamon1231@hotmail.com>
2013-05-13 18:59:58 +0800

    updatd

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-job_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rake :task --silent :output"
-job_type :script, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec script/:task :output"
-job_type :runner, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rails runner ':task' :output"
+#job_type :rake, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rake :task --silent :output"
+#job_type :script, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec script/:task :output"
+#job_type :runner, "{ cd #{@current_path} > /dev/null; } && RAILS_ENV=:environment bundle exec rails runner ':task' :output"
 
 every :hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
   runner "Reminder.send_message"
